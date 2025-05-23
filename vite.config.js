d088aace564878b10bf26c00d4b253e6be86162f
Francesco Cassano <fracasso96@hotmail.it>
2025-05-23 12:00:30 +0200

    modified vite file

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
   build: {
     lib: {
       entry: 'src/component-entry.jsx',
       name: 'ConceptMap',
       fileName: () => 'concept-map.js',
-      formats: ['es']
-    },
+      formats: ['es'],
+    }
   }
 });
